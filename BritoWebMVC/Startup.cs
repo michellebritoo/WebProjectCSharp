cb4eb064fb9af3ec57f47f9bc03cddde38218669
Michelle Brito <michele.brito14@gmail.com>
2020-11-30 14:36:59 -0300

    Seeding service

--- BritoWebMVC/Startup.cs
+++ BritoWebMVC/Startup.cs
@@ -39,14 +39,17 @@ namespace BritoWebMVC
             services.AddDbContext<BritoWebMVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("BritoWebMVCContext"), 
                     builder => builder.MigrationsAssembly("BritoWebMVC")));
+
+            services.AddScoped<SeedingService>(); //register this service in aplication
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
