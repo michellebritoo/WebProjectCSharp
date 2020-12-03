fed4ecc48064fa4cdc2ffed04bc3d29a42395e08
Michelle Brito <michele.brito14@gmail.com>
2020-12-03 11:27:01 -0300

    Asynchronous operations using Task

--- BritoWebMVC/Services/DepartmentService.cs
+++ BritoWebMVC/Services/DepartmentService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using BritoWebMVC.Data;
 using BritoWebMVC.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace BritoWebMVC.Services
 {
@@ -15,9 +17,9 @@ namespace BritoWebMVC.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
